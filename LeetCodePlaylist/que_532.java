288207afc5ce955ce353eaa40f9d2910ca146692
gaurav-3612 <gauravsharma3612@gmail.com>
2025-09-09 22:50:22 +0530

    Removed unwanted files

--- LeetCodePlaylist/que_532.java
+++
@@ -1,6 +0,0 @@
-package LeetCodePlaylist;
-
-public class que_532 {
-    // used hashmap to store values and their frequencies
-    // used the concept that if k>0 and hashMap contains value+k then it means difference exists
-}
--- LeetCodePlaylist/question_48.java
+++
@@ -1,7 +0,0 @@
-package LeetCodePlaylist;
-
-public class question_48 {
-    // make two functions
-    // 1-- transpose of matrix (reverse(i,j) with (j,i))
-    // 2-- reverse every row (two pointers left and right swap them until left<right)
-}
